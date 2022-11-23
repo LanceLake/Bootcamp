c020d6a88a4e70a5a15535429046063a60d3b546
Lance_Lake <temp@MostDeadlyGame.com>
2022-11-22 16:00:55 -0800

    Mondays work

--- week6-Server-Side-APIs/01-Activities/10-Stu_Demo_Dynamic/Unsolved/assets/js/script.js
+++ week6-Server-Side-APIs/01-Activities/10-Stu_Demo_Dynamic/Unsolved/assets/js/script.js
@@ -12,6 +12,14 @@ function getApi() {
       // Use the console to examine the response
       console.log(data);
       // TODO: Loop through the data and generate your HTML
+		for (i = 0;i < data.length; i++){
+
+			var userName = document.createElement('h3');
+			userName.textContent = 'User: ' + data[i].login + ' URL: ' + data[i].html_url;
+
+			userContainer.append(userName);
+		}
+
     });
 }
 fetchButton.addEventListener('click', getApi);

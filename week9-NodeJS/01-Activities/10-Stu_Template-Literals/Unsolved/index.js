033fab7fd8f5408770a4becb2d7e36127733c01f
Lance_Lake <temp@MostDeadlyGame.com>
2022-12-15 19:29:13 -0800

    Thursdays Work

--- week9-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
+++ week9-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
@@ -1,11 +1,16 @@
 // music should be an object with title, artist, and album properties
 const music = {
-  // code here
+  Title: "Yellow Submarine",
+  Artist: "Beatles",
+  Album: "One of their albums"
  
 };
 
 // Write code between the backticks tags to output the data from the music object above.
-const songSnippet = ``;
+const songSnippet = 
+`The Title is ${music.Title}.
+The Artist is ${music.Artist}.
+The Album is ${music.Album}.`;
 
-console.log(songSnippet)
+console.log(songSnippet);
 
---
+++ week9-NodeJS/01-Activities/12-Stu_Parameter-Check/index.js
@@ -0,0 +1,9 @@
+const inputA = process.argv[2];
+const inputB = process.argv[3];
+
+if(inputA == inputB) {
+	console.log("This matches");
+} else {
+	console.log("This does not match");
+};
+

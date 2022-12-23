97549c614fbe0c0df22194144f1ed6fdd3970a78
Lance_Lake <temp@MostDeadlyGame.com>
2022-12-22 19:26:08 -0800

    Thursdays Work

--- week10-OOP/01-Activities/16-Stu_Organizing-Tests/Unsolved/test/dayCare.test.js
+++ week10-OOP/01-Activities/16-Stu_Organizing-Tests/Unsolved/test/dayCare.test.js
@@ -15,16 +15,16 @@ describe("DayCare", () => {
   describe("addChild", () => {
     it("should add a child to the 'children' array", () => {
       // TODO: Add a comment describing the purpose of the following declarations
-	  // Answer: 
+	  // Answer: Create new objects to play with.
       const child = new Child("Tammy", 1);
       const dayCare = new DayCare();
 
       // TODO: Add a comment describing the purpose of the following method
-	  // Answer: 
+	  // Answer: Add a new child to the daycare object
       dayCare.addChild(child);
 
       // TODO: Add a comment describing the purpose of the following statements
-	  // Answer: 
+	  // Answer: Verify that the child was added to the array.
       expect(dayCare.children.length).toEqual(1);
       expect(dayCare.children[0]).toBe(child);
     });
@@ -36,7 +36,7 @@ describe("DayCare", () => {
       dayCare.addChild(child);
 
       // TODO: Add a comment describing the purpose of the following statement
-	  // Answer: 
+	  // Answer: Verify that the child was not added to the array.
       expect(dayCare.children.length).toEqual(0);
     });
 
@@ -45,7 +45,7 @@ describe("DayCare", () => {
       const child = new Child("Alice", 4);
 
       // TODO: Add a comment describing the purpose of the following expression
-	  // Answer: 
+	  // Answer: Add 3 additional children to the daycare array.
       dayCare.children = [
         new Child("Tammy", 1),
         new Child("Mark", 2),
@@ -55,25 +55,26 @@ describe("DayCare", () => {
       dayCare.addChild(child);
 
       // TODO: Add a comment describing the purpose of the following statement
-	  // Answer: 
+	  // Answer: Check to make sure there are 3 children in the array and not 4 since we used the 'addChild' function and there is a 3 child limit built in.
       expect(dayCare.children.length).toEqual(3);
     });
 
     it("should throw an error if not provided a Child object as an argument", () => {
       // TODO: Add a comment describing the purpose of the following declaration
+	  // Answer: Define an error to check if the error is the same as below.
       const err = new Error(
         "Expected parameter 'child' to be an instance of Child"
       );
 
       // TODO: Add a comment describing the purpose of the following expression
-	  // Answer: 
+	  // Answer: Arrange the error to trigger.
       const cb = () => {
         const dayCare = new DayCare();
         dayCare.addChild();
       };
 
       // TODO: Add a comment describing the purpose of the following statement
-	  // Answer: 
+	  // Answer: Verify that the error is correct.
       expect(cb).toThrowError(err);
     });
   });
@@ -87,14 +88,14 @@ describe("DayCare", () => {
       dayCare.children = [child1, child2, child3];
 
       // TODO: Add a comment describing the purpose of the following declaration
-	  // Answer: 
+	  // Answer: Remove a child from the array on daycare.
       const removed = dayCare.pickupChild(child2.name);
 
       expect(removed).toBe(child2);
       expect(dayCare.children.length).toEqual(2);
 
       // TODO: Add a comment describing the purpose of the following statement
-	  // Answer: 
+	  // Answer: verify that child2 is not in the array.
       expect(
         dayCare.children.some(child => child.name === child2.name)
       ).toEqual(false);
@@ -108,15 +109,15 @@ describe("DayCare", () => {
       dayCare.children = [child1, child2, child3];
 
       // TODO: Add a comment describing the purpose of the following declaration
-	  // Answer: 
+	  // Answer: Attempt to pick up a child that doesn't exist.
       const removed = dayCare.pickupChild("Fred");
 
       // TODO: Add a comment describing the purpose of the following statement
-	  // Answer: 
+	  // Answer: Verify that the returned object is undefined.
       expect(typeof removed).toEqual("undefined");
 
       // TODO: Add a comment describing the purpose of the following statement
-	  // Answer: 
+	  // Answer: Verify that no object is removed from the array.
       expect(dayCare.children).toEqual([child1, child2, child3]);
     });
   });

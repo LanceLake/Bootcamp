1acc40d01da4b824446391d53b0ecdfc9121b456
Lance_Lake <temp@MostDeadlyGame.com>
2023-01-17 18:51:19 -0800

    Tuesdays Work

--- week12-SQL/01-Activities/14-Stu_Data-Types/Unsolved/db/schema.sql
+++ week12-SQL/01-Activities/14-Stu_Data-Types/Unsolved/db/schema.sql
@@ -4,8 +4,8 @@ CREATE DATABASE customer_db;
 USE customer_db;
 
 CREATE TABLE customers (
-  id,
-  first_name,
-  last_name,
-  value_card_member
+  id int NOT NULL,
+  first_name varchar(50) NOT NULL,
+  last_name varchar(50) NOT NULL,
+  value_card_member Boolean NOT NULL
 );

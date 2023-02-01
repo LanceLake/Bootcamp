b7eb05c89a2ffcabd972e6fdfe2cb6bc450d1f15
Lance_Lake <temp@MostDeadlyGame.com>
2023-01-31 22:29:14 -0500

    Tuesdays Work

--- week14-MVC/01-Activities/14-Stu_Custom-Helpers/Unsolved/utils/helpers.js
+++ week14-MVC/01-Activities/14-Stu_Custom-Helpers/Unsolved/utils/helpers.js
@@ -4,4 +4,11 @@ module.exports = {
   },
   // TODO: Create a custom helper 'format_date' that takes in a timestamp,
   // adds five years to the date, and formats it as M/D/YYYY
+
+  format_date: (date) => {
+
+	const initialDate =  new Date(date);
+
+    return `${initialDate.getMonth()}/${initialDate.getDay()}/${initialDate.getFullYear() + 5}`;
+  },
 };

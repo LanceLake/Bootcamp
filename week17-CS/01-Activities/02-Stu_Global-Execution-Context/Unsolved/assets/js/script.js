c1fe6e5b4111f2fb9b01c5397cf5c16a0c7fd2a7
Lance_Lake <temp@MostDeadlyGame.com>
2023-02-21 22:29:07 -0500

    Tuesdays Work

--- week17-CS/01-Activities/02-Stu_Global-Execution-Context/Unsolved/assets/js/script.js
+++ week17-CS/01-Activities/02-Stu_Global-Execution-Context/Unsolved/assets/js/script.js
@@ -1,25 +1,25 @@
 // 1) Where is carNoise stored?
-//
+// Global execution context (GEC)
 const carNoise = 'Honk';
 // 2) Where is goFast stored?
-// 
+// Global execution context (GEC)
 const goFast = speed => {
-  // 4) When is speed assigned a value? Where is this value stored?
-  // 
-  
-  // 5) Where is makeNoise stored?
-  //
-  const makeNoise = sound => {
-    console.log(`My speed is at ${speed}, time to ${sound}`);
-  }
+	// 4) When is speed assigned a value? Where is this value stored?
+	// When goFast is invoked, the argument of the function is assigned to the paramater shown in the functional execution context of goFast.
 
-  // 6) What happens in the following statement?
-  // 
-  makeNoise(carNoise);
+	// 5) Where is makeNoise stored?
+	// makeNoise is stored in the function execution context of goFast
+	const makeNoise = sound => {
+		console.log(`My speed is at ${speed}, time to ${sound}`);
+	}
+
+	// 6) What happens in the following statement?
+	// makeNoise is placed on the call stack which creates a new function execution.
+	makeNoise(carNoise);
 }
 
 // 3) What happens in the following statement?
-// 
+// Puts up an alert asking if you want to go fast. If you click ok, run the function "goFast" and pass in 80. Confirm is a web API and when done, it places goFast on the stack.
 if(confirm("Do you want to go fast?")) {
-  goFast(80);
+	goFast(80);
 }
--- week17-CS/01-Activities/10-Stu_Callback-Queue/Unsolved/assets/js/script.js
+++ week17-CS/01-Activities/10-Stu_Callback-Queue/Unsolved/assets/js/script.js
@@ -1,9 +1,14 @@
+const button = document.querySelector('btn');
+
 // function to block the stack for x number of milliseconds
 const pause = milliSeconds => {
-  // get the current time
-  var startTime = new Date().getTime(); 
-  // block stack until time's up
-  while (new Date().getTime() < startTime + milliSeconds); 
-}
+	// get the current time
+	var startTime = new Date().getTime(); 
+	// block stack until time's up
+	setTimeout(() => {
+		document.getElementById('submitbutton').removeAttribute('disabled');
+	}, milliSeconds);
+
+};
 
 pause(8000);
